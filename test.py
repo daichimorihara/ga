90f04a598129dc7069b6bd04287ddf109fd5a5d2
Daichi Morihara <daichi16daichi04@icloud.com>
2024-02-13 14:32:55 +0900

    failed test

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ class TestCalculations(unittest.TestCase):
         self.calculations = Calculations(8, 2)
 
     def test_sum(self):
-        self.assertEqual(self.calculations.get_sum(), 10, "The sum is wrong.")
+        self.assertEqual(self.calculations.get_sum(), 1, "The sum is wrong.")
 
     def test_diff(self):
         self.assertEqual(self.calculations.get_difference(), 6, "The difference is wrong.")
